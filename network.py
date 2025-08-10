ffe04b1cf0d0ca3e6dca69d827c93424d8de0ac7
Vinh Nub <vinhnguyenvan1255@gmail.com>
2025-08-10 12:49:03 +0700

    fixing udp threading

--- network.py
+++ network.py
@@ -3,17 +3,17 @@ import pickle
 
 class NetWork():
     def __init__(self, serverIp):
-        self._client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server = serverIp
         self.port = 5555
         self.addr = (self.server, self.port)
 
-    def send(self, data):
-        try:
-            self._client.sendto(pickle.dumps(data), self.addr)
-            responseData, self.server = self._client.recvfrom(4096)
-            result = pickle.loads(responseData)
-            return result
-        except socket.error as e:
-            print(e)
+    # def send(self, data):
+    #     try:
+    #         self.client.sendto(pickle.dumps(data), self.addr)
+    #         responseData, self.server = self.client.recvfrom(4096)
+    #         result = pickle.loads(responseData)
+    #         return result
+    #     except socket.error as e:
+    #         print(e)
 
